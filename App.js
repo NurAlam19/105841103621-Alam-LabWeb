4f44ebbc7be078a3ede664c23c32ba7233db4d59
NurAlam19 <105841103621@student.unismuh.ac.id>
2024-06-14 21:29:20 +0800

    box-view

--- App.js
+++ App.js
@@ -4,24 +4,50 @@ import React from 'react'
 const App = () => {
   return (
     <View style ={{
+      marginTop :400
+      }}>  
+    <View style ={{
         flex :1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginLeft : 70
       }}>
       <View style={{
-        width:250,
-        height:100,
+        width:100,
+        height:50,
         backgroundColor:'red',
         borderRadius : 20,
-        justifyContent : 'center'
+        justifyContent : 'center',
       }}>
         <Text style ={{
           fontSize :20,
           color : 'white',
           textAlign : 'center',
-          fontWeight : 'bold'
+          fontWeight : 'bold',
         }}>Login</Text>
       </View>
+    </View>
+    <View style ={{
+          flex : 1,
+          jutifyContent : 'center',
+          alignItems : 'flex-end',
+          marginRight : 70
+    }}>
+      <View style ={{
+          width :100,
+          height :50,
+          backgroundColor : 'blue',
+          borderRadius : 20,
+          justifyContent : 'center',
+      }}>
+        <Text style ={{
+          fontSize :20,
+          color : 'white',
+          textAlign : 'center',
+          fontWeight : 'bold',
+        }}>Sing Up</Text>
+      </View>
+    </View>
     </View>
   )
 }
