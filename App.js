e73317e90f2763161b2ff0cab563204dda6a17fa
NurAlam19 <105841103621@student.unismuh.ac.id>
2024-06-24 20:56:32 +0800

    Costum Komponen

--- App.js
+++ App.js
@@ -1,53 +1,44 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+
+const ButtonCostum = ({text,color}) => {
+  return (
+    <View style ={{
+      backgroundColor : color,
+      width : 250,
+      height : 100,
+      borderRadius : 10,
+      justifyContent : 'center',
+      marginBottom :10
+    }}>
+      <Text style ={{
+        textAlign : 'center',
+        color : 'white',
+        fontSize : 30,
+        fontWeight : 'bold',
+      }}>
+        {text}
+      </Text>
+    </View>
+  )
+}
+
+const TextInputCostum =({placeholder, color, typeKeybooard}) => {
+  return(
+    <TextInput></TextInput>
+  )
+}
 
 const App = () => {
   return (
     <View style ={{
-      marginTop :400
-      }}>  
-    <View style ={{
-        flex :1,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        marginLeft : 70
-      }}>
-      <View style={{
-        width:100,
-        height:50,
-        backgroundColor:'red',
-        borderRadius : 20,
-        justifyContent : 'center',
-      }}>
-        <Text style ={{
-          fontSize :20,
-          color : 'white',
-          textAlign : 'center',
-          fontWeight : 'bold',
-        }}>Login</Text>
-      </View>
-    </View>
-    <View style ={{
-          flex : 1,
-          jutifyContent : 'center',
-          alignItems : 'flex-end',
-          marginRight : 70
+      flex : 1,
+      justifyContent : 'center',
+      alignItems : 'center',
     }}>
-      <View style ={{
-          width :100,
-          height :50,
-          backgroundColor : 'blue',
-          borderRadius : 20,
-          justifyContent : 'center',
-      }}>
-        <Text style ={{
-          fontSize :20,
-          color : 'white',
-          textAlign : 'center',
-          fontWeight : 'bold',
-        }}>Sing Up</Text>
-      </View>
-    </View>
+      <ButtonCostum text = "LOGIN" color = "red" />
+      <ButtonCostum text = "LOGIN" color = "orange" />
+      <ButtonCostum text = "REGIS" color = "purple" />
     </View>
   )
 }
